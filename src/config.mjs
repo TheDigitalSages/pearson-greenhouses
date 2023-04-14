6c48ad9996035ecbd65dd4df2f81bca48edb6f02
Edward Beazer <ebeazer@thedigitalsages.com>
2023-04-14 13:40:48 -0400

    Finished base tempalte

--- src/config.mjs
+++ src/config.mjs
@@ -23,40 +23,8 @@ const CONFIG = {
     day: "numeric",
     timeZone: "UTC",
   }),
-
-  googleAnalyticsId: false, // or "G-XXXXXXXXXX",
-  googleSiteVerificationId: "",
-
-  blog: {
-    disabled: true,
-    postsPerPage: 4,
-
-    post: {
-      permalink: "/%slug%", // Variables: %slug%, %year%, %month%, %day%, %hour%, %minute%, %second%, %category%
-      noindex: false,
-      disabled: false,
-    },
-
-    list: {
-      pathname: "blog", // Blog main path, you can change this to "articles" (/articles)
-      noindex: false,
-      disabled: false,
-    },
-
-    category: {
-      pathname: "category", // Category main path /category/some-category
-      noindex: true,
-      disabled: false,
-    },
-
-    tag: {
-      pathname: "tag", // Tag main path /tag/some-tag
-      noindex: true,
-      disabled: false,
-    },
-  },
 };
 
 export const SITE = { ...CONFIG, blog: undefined };
-export const BLOG = CONFIG.blog;
+
 export const DATE_FORMATTER = CONFIG.dateFormatter;
--- src/data.js
+++ src/data.js
@@ -1,74 +1,11 @@
-import { getPermalink, getBlogPermalink, getAsset } from "./utils/permalinks";
-
-export const headerData = {
-  links: [
-    {
-      text: "Landing",
-      links: [
-        {
-          text: "Sass",
-          href: getPermalink("/landing/saas"),
-        },
-        {
-          text: "Startup",
-          href: getPermalink("/landing/startup"),
-        },
-        {
-          text: "Mobile App",
-          href: getPermalink("/landing/mobile-app"),
-        },
-      ],
-    },
-    {
-      text: "Pages",
-      links: [
-        {
-          text: "Features",
-          href: "#",
-        },
-        {
-          text: "Pricing",
-          href: "#",
-        },
-        {
-          text: "About us",
-          href: "#",
-        },
-        {
-          text: "Contact",
-          href: "#",
-        },
-        {
-          text: "Terms",
-          href: getPermalink("/terms"),
-        },
-        {
-          text: "Privacy policy",
-          href: getPermalink("/privacy"),
-        },
-      ],
-    },
-    {
-      text: "Widgets",
-      href: "#",
-    },
-    {
-      text: "Blog",
-      href: getBlogPermalink(),
-    },
-  ],
-  actions: [{ type: "button", text: "Download", href: "https://github.com/onwidget/astrowind" }],
-};
+import { getAsset } from "./utils/permalinks";
 
 export const footerData = {
   links: [],
   secondaryLinks: [],
   socialLinks: [
-    { ariaLabel: "Twitter", icon: "tabler:brand-twitter", href: "#" },
     { ariaLabel: "Instagram", icon: "tabler:brand-instagram", href: "#" },
     { ariaLabel: "Facebook", icon: "tabler:brand-facebook", href: "#" },
-    { ariaLabel: "RSS", icon: "tabler:rss", href: getAsset("/rss.xml") },
-    { ariaLabel: "Github", icon: "tabler:brand-github", href: "https://github.com/onwidget/astrowind" },
   ],
   footNote: `
     Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://edwardbeazer.com/"> The Digital Sages</a> · All rights reserved.
